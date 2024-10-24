0cd4f567b9ad154ae7cc709e67d995f441ff004d
Imbecile426 <156496909+Imbecile426@users.noreply.github.com>
2024-10-24 19:41:11 +0530

    lol

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#Button1').hover(function(){
+        $(this).text("Yes");
+        $(this).css("background-color","rgb(54, 255, 47)");
+        $('#Button2').text("No");
+        $('#Button2').css("background-color","red");
+    });
+    $('#Button2').hover(function(){
+        $(this).text("Yes");
+        $(this).css("background-color","rgb(54, 255, 47)");
+        $('#Button1').text("No");
+        $('#Button1').css("background-color","red");
+    });
+    $("button").click(function(){
+        alert("I KNEW IT! &#129392;");
+    })
+})
